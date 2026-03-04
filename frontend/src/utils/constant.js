f6f75e9ef2ba1ea21f0bda25ba23d7b30670a5be
Ishan Amrit Srivastava <98459119+IshanAmrit28@users.noreply.github.com>
2026-03-05 02:08:17 +0530

    fixing connection issue

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,11 +4,12 @@
 // export const COMPANY_API_END_POINT="https://jobboard-iscj.onrender.com/api/v1/company/";
 
 
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user/";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job/";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application/";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company/";
+import { API_BASE_URL } from '../constants';
 
+export const USER_API_END_POINT=`${API_BASE_URL}/api/v1/user/`;
+export const JOB_API_END_POINT=`${API_BASE_URL}/api/v1/job/`;
+export const APPLICATION_API_END_POINT=`${API_BASE_URL}/api/v1/application/`;
+export const COMPANY_API_END_POINT=`${API_BASE_URL}/api/v1/company/`;
 // --- MOCK DATA START ---
 
 // Companies Mock Data
